b36676bf1f98cd30bdc4df73fefd065419140698
Frederik Dengler <frederikdengler@gmail.com>
2018-01-25 18:55:33 +0100

    update project

--- console.php
+++ console.php
@@ -1,141 +1,50 @@
 <?php
 
-const P = "\n";
-const DP = "\n\n";
-define('MODULES_ROOT', __DIR__ . '/modules');
-define('CONFIGS_ROOT', __DIR__ . '/configs');
+require_once 'vendor/autoload.php';
 
-echo P . colorize("Smart cli tools. Smart is a magic mirror software for", "NOTE");
-echo P . colorize("everyone. For all available functions use -listfunctions", "NOTE") . DP;
+use splitbrain\phpcli\CLI;
+use splitbrain\phpcli\Options;
 
-if(empty($argv[1])) {
-    echo "\n\nNo Command given please look in the list below:\n\n";
-    listFunctions();
-}else {
-    $callFunction = str_replace('-' , '', $argv[1]);
-    $callFunction = ucwords($callFunction);
-    call_user_func($callFunction, $argv);
-    echo DP;
-}
-
-function listFunctions() {
-    echo "All functions in this script: " . P;
-
-    $functions = [
-        '-install <modulname> <version>',
-        '-listmodules'
-    ];
-
-    foreach ($functions as $function) {
-        if(!in_array($function, ['colorize'])) {
-            echo "\t" . $function . P;
-        }
-    }
-}
-
-function listModules()
+class Minimal extends CLI
 {
-    $modules = file_get_contents(CONFIGS_ROOT . '/modules.json');
-
-    foreach (json_decode($modules, true) as $module) {
-        echo colorize($module['module'], "NOTE") . P;
-        foreach ($module['releases'] as $release) {
-            echo 'Release: ' . $release['release'] . DP;
-        }
-    }
-}
-
-function install($argv) {
-    echo "Install new Module" . DP;
-
-    if(empty($argv[2]) || empty($argv[3])) {
-        echo "Please provide <modulename> and <version>!" . P;
-        return;
+    // register options and arguments
+    protected function setup(Options $options)
+    {
+        $options->setHelp('Smart-Core a modular system for displaying smart widgets on a magic mirror.');
+        $options->registerOption('version', 'print version', 'v');
+        $options->registerCommand('modules', 'handle modules');
+        $options->registerArgument('install', 'installs a module', false, 'modules');
+        $options->registerArgument('list', 'list all modules', false, 'modules');
     }
 
-    $moduleName = $argv[2];
-    $moduleSource = 'https://github.com/smartwebtools/' . $moduleName . '/archive/' . $argv[3] . '.zip';
+    // implement your code
+    protected function main(Options $options)
+    {
+        $args = $options->getArgs();
 
-    echo "Module: " . $argv[2] . P;
-    echo "Version: " . $argv[3] . P;
-
-    //TODO Test url before removing anything
-
-    if(is_dir(MODULES_ROOT . '/' . $moduleName)) {
-        echo colorize("DIRECTORY NOT EMPTY IT WILL BE REMOVED NOW!", "NOTE") . DP;
-        rrmdir(MODULES_ROOT . '/' . $moduleName);
-    }
-
-    downloadZipFile($moduleSource, $moduleName);
-    extractModule($moduleName);
-
-    if(is_dir(MODULES_ROOT . '/' . $moduleName . '-' . $argv[3])) {
-        rename(MODULES_ROOT . '/' . $moduleName . '-' . $argv[3], MODULES_ROOT . '/' .  $moduleName);
-    }
-
-    echo colorize($moduleName . " was installed successfully!", "SUCCESS") . P;
-
-    unlink(MODULES_ROOT . '/' .  $moduleName . '.zip');
-}
-function downloadZipFile($url, $module)
-{
-    $fh = fopen(MODULES_ROOT . '/' . $module . '.zip', 'w');
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_FILE, $fh);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // this will follow redirects
-    curl_exec($ch);
-    curl_close($ch);
-    fclose($fh);
-    echo colorize("Download of module: " . $module . " successful!", "SUCCESS") . P;
-}
-function extractModule($module)
-{
-    $zip = new ZipArchive();
-    $file = $zip->open(MODULES_ROOT . '/' . $module . '.zip');
-    if ($file === TRUE) {
-        $zip->extractTo(MODULES_ROOT . '/');
-        $zip->close();
-        echo colorize("Extract of module: " . $module . " successful!", "SUCCESS") . P;
-    }else {
-        echo colorize("Extract of module: " . $module . " failed!", "FAILURE") . P;
-    }
-}
-
-//HELPER FUNCTIONS
-function colorize($text, $status) {
-    $out = "";
-    switch($status) {
-        case "SUCCESS":
-            $out .= "[32m"; //Green background
-            break;
-        case "FAILURE":
-            $out .= "[41m"; //Red background
-            break;
-        case "WARNING":
-            $out .= "[43m"; //Yellow background
-            break;
-        case "NOTE":
-            $out .= "[94m"; //Blue background
-            break;
-        default:
-            new \Exception("Invalid status: " . $status);
-    }
-    return chr(27) . "$out" . "$text" . chr(27) . "[0m";
-}
-function rrmdir($dir) {
-    if (is_dir($dir)) {
-        $objects = scandir($dir);
-        foreach ($objects as $object) {
-            if ($object != "." && $object != "..") {
-                if (is_dir($dir."/".$object))
-                    rrmdir($dir."/".$object);
-                else
-                    unlink($dir."/".$object);
+        if ($options->getCmd() == 'modules') {
+            if ($options->getOpt('install')) {
+                $this->success('Module ' . $args[0] . 'wird jetzt installiert');
+            } else if ($options->getOpt('list')) {
+                $this->info('This is a list of all modules');
             }
         }
-        rmdir($dir);
+
+        if ($options->getOpt('version')) {
+            $this->info('1.0.0');
+        }
+
+        //echo $options->help();
     }
 }
+
+// execute it
+$cli = new Minimal();
+$cli->run();
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 exit();
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,39 +1,21 @@
+var artyom = new Artyom();
+
 (function () {
     console.log('Init smart');
     
-    var artyom = new Artyom();
-    var commandHello = {
-        indexes:["hallo","Guten morgen"], // These spoken words will trigger the execution of the command
-        action:function(){ // Action to be executed when a index match with spoken word
-            artyom.say("Hallo was kann ich für dich tun?");
-        }
-    };
-    
-    artyom.addCommands(commandHello); // Add the command with addCommands method. Now
-    
-    artyom.initialize({
-        lang:"de-DE",// A lot of languages are supported. Read the docs !
-        continuous:true,// recognize 1 command and stop listening !
-        listen:false, // Start recognizing
-        debug:true, // Show everything in the console
-        obeyKeyword: 'Hör mir zu',
-        speed:1 // talk normally
-    }).then(function(){
-        console.log("Ready to work !");
-    });
-
+    //SYSTEM OBJECT START//
     System = {
         moduleConfig: function (moduleName) {
             let config = null;
-
+            
             $.getJSON({'url': '/modules/' + moduleName + '/config.json', 'async': false}, function (data) {
                 config = data;
             });
-
+            
             return config;
         },
         log: function (message, priority = 'info') {
-            switch(priority) {
+            switch (priority) {
                 case "info":
                     console.info(message);
                     break;
@@ -41,6 +23,41 @@
                     console.error(message);
                     break;
             }
+        },
+        updateInfo: function () {
+            return false;
         }
     };
+    //SYSTEM OBJECT END//
+    
+    var systemCommands = [{
+        indexes: ["Gibt es Module Updates", "Gibt es irgendwelche Updates"],
+        action: function () {
+            let updateAvailable = System.updateInfo();
+            
+            if(updateAvailable) {
+                artyom.say("Es gibt neue Updates für dich!");
+            }else {
+                artyom.say("Momentan gibt es keine neuen updates für dich!");
+            }
+        }
+    }, {
+        indexes: ["Erzähl mir einen Witz", "Guten morgen"],
+        action: function () {
+            artyom.say("Hallo was kann ich für dich tun?");
+        }
+    }];
+    
+    artyom.addCommands(systemCommands); // Add the command with addCommands method. Now
+    
+    artyom.initialize({
+        lang: "de-DE",
+        continuous: false,
+        listen: false,
+        debug: true,
+        //obeyKeyword: 'Smart',
+        speed: 1
+    }).then(function () {
+        console.log("Ready to work !");
+    });
 })();
